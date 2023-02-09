e8c34fac051e44299b8950e57792de573111417d
DZE <daniel.zenke@hyperlink.eu>
2023-02-09 12:00:41 +0100

    Finished comments feature and started with newsletter api

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,20 +1,25 @@
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+@props(['comment'])
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60"
+                 class="rounded-xl">
+        </div>
 
-            <p class="text-xs">
-                Posted
-                <time>8 months ago</time>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam asperiores delectus, doloremque facere id maxime, minus nisi nostrum pariatur perferendis provident quos, repellendus sed vero voluptates. Accusamus, ea?
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
